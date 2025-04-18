20db5940c4ab42aef8224b1265ebb5c6210bcbff
Ambika <145963774+AjIndia77@users.noreply.github.com>
2025-04-18 20:16:17 +0530

    Create FlattenDoublyll.cpp

---
+++ Medium/FlattenDoublyll.cpp
@@ -0,0 +1,32 @@
+//Medium - 430 Flatten a multilevel doubly linked list
+class Solution {
+public:
+    Node* flatten(Node* head) {
+        if(head==NULL){
+            return head;
+            }
+    Node* curr = head;
+    while(curr != NULL){
+        if(curr->child!=NULL){
+            //flatten the child node
+            Node* next = curr->next;
+            curr->next= flatten(curr->child);
+
+            curr->next->prev = curr;
+            curr->child = NULL;
+
+            //find tail
+            while(curr->next!=NULL){
+                curr=curr->next;
+            }
+
+            //attach tail with next ptr
+            if(next!=NULL){
+                curr->next = next;
+                next->prev = curr;
+            }
+        }
+        curr = curr->next;
+    }
+         return head;
+    }
