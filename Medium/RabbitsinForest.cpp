ff7eefcccccf07e9906e46783b37f9eaabd1d45b
Ambika <145963774+AjIndia77@users.noreply.github.com>
2025-04-20 22:39:08 +0530

    Update RabbitsinForest.cpp

--- Medium/RabbitsinForest.cpp
+++ Medium/RabbitsinForest.cpp
@@ -1,1 +1,19 @@
-
+//Medium - 781 Rabbits in forest
+class Solution {
+    public:
+    int numRabbits(vector<int>& answers) {
+        unordered_map<int,int>mp;
+        for(int &x : answers) {
+            mp[x]++;
+        }
+        int total=0;
+        for(auto &it :mp){
+            int x = it.first;//answer
+            int count=it.second;//how many rabbits answered x
+            int groupSize=(x+1);
+            int groups=ceil((double)count/groupSize);
+            total += groups*(groupSize);
+        }
+        return total;
+    }
+};
