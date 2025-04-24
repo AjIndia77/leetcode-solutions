c5e2fa138a09139d4f37acecc47ab62327cabd6f
Ambika <145963774+AjIndia77@users.noreply.github.com>
2025-04-24 20:59:07 +0530

    Update MajorityElement.cpp

--- Easy/MajorityElement.cpp
+++ Easy/MajorityElement.cpp
@@ -1,1 +1,20 @@
+//Easy - 169 Majority Element solved by applying Brute force approach
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        for(int val : nums){
+            int freq = 0;
+            for(int el : nums){
+                if(el == val){
+                    freq++;
 
+                }
+            }
+            if(freq > n/2){
+                return val;
+            }
+        }
+        return -1;
+    }
+};
